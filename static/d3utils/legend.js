66b288a1204d000172b9419175da45015c6c8c0d
Vineet <naikvin@gmail.com>
2013-08-10 00:17:52 +0530

    Make d3utils.legend more flexible to work with

--- static/d3utils/legend.js
+++ static/d3utils/legend.js
@@ -1,16 +1,18 @@
 (function (d3, d3utils, _, undefined) {
     "use strict";
 
-    d3utils.legend = function (keys, values, colors) {
-        var $legend = $('<ul class="legend"></ul>');
-        $legend.html(_.map(keys, function (d, i) {
-            return '<li><div style="background: '+colors.fill(d, i)+'"></div>'+keys[i]+' <span>'+values[i]+'</span></li>';
-        }));
-        // adjustments to fit in the graph and legend side by side
-        $(".graph > svg").css({
-            'float': 'left',
-            'margin-right': '7%'
-        }).after($legend);
+    d3utils.legend = function ($graph) {
+        return function (keys, values, colors) {
+            var $legend = $('<ul class="legend"></ul>');
+            $legend.html(_.map(keys, function (d, i) {
+                return '<li><div style="background: '+colors.fill(d, i)+'"></div>'+keys[i]+' <span>'+values[i]+'</span></li>';
+            }));
+            // adjustments to fit in the graph and legend side by side
+            $("svg", $graph).css({
+                'float': 'left',
+                'margin-right': '10px'
+            }).after($legend);
+        };
     };
 
 }) (d3, d3utils, _);
